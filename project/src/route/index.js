b3486eea3391d584d9a8df5ee32509354d9a9fb0
wbq <1500736215@qq.com>
2023-12-09 21:03:14 +0800

    提交

--- project/src/route/index.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import routers from './modules/index'
-
-const router = new createRouter({
-    history: createWebHistory(),
-    routes: routers
-})
-
-export default router
